b1735ec67f20ea6b7450184346498ee87e43ce7b
Satantra Tewari <satantra1508@gmail.com>
2020-11-26 19:03:51 +0530

    [satantra] ability to update employee details

--- js/HomePage.js
+++ js/HomePage.js
@@ -1,10 +1,12 @@
 let employeePayrollList;
+
 //Event listener when HTML page contents are loaded
 window.addEventListener('DOMContentLoaded',(event)=>
     {
         employeePayrollList = GetEmployeeDataLocalStorage();
         document.querySelector('.emp-count').textContent = employeePayrollList.length;
         CreateInnerHTML();
+        localStorage.removeItem('EditEmployee');
     }
 );
 
@@ -42,13 +44,22 @@ function CreateInnerHTML()
 //Remove employee from table
 function remove(node)
 {
-    let empData = employeePayrollList.find(emp=>emp._id == node._id);
+    let empData = employeePayrollList.find(emp=>emp._id == node.id);
     if(!empData) return;
     let index = employeePayrollList.map(emp=>emp._id).indexOf(empData._id);
     employeePayrollList.splice(index,1);
     localStorage.setItem('EmployeePayrollList',JSON.stringify(employeePayrollList));
     document.querySelector('.emp-count').textContent = employeePayrollList.length;
     CreateInnerHTML();
+}
+
+//Update data
+function update(node)
+{
+    let empData = employeePayrollList.find(emp=>emp._id == node.id);
+    if(!empData) return;
+    localStorage.setItem('EditEmployee',JSON.stringify(empData));
+    window.location.replace("../pages/AddEmployeeForm.html");
 }
 
 //Get departments for a employee to display on HTML page
